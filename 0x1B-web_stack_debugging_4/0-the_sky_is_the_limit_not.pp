c77ab4d18f2d0dd9943618745341e4980d6bf45a
root <mutwirited@gmail.com>
2022-11-09 06:01:12 +0000

    up

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,16 @@
+# add
+
+package {'nginx':
+	ensure => installed,
+}
+
+file { '/etc/default/nginx':
+	ensure => file,
+	content => 'ULIMIT="-n 15000"',
+}
+
+exec { 'restart':
+	require => File['/etc/default/nginx'],
+	command => 'service nginx restart',
+	path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ]
+}
